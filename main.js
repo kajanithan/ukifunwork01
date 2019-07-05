de31fb401df921187b2445e367f410075bc72667
kajanithan <mkajanithan@gmail.com>
2019-07-05 15:15:16 +0530

    uki funwork

--- main.js
+++ main.js
@@ -47,4 +47,64 @@ $(document).ready(function() {  SC.stream('/tracks/315381087',function(sound){
             sound.stop();
           });
    });
+   $(document).ready(function() {  SC.stream('/tracks/89702917',function(sound){
+       $('#start5').click(function(e) {
+             e.preventDefault();
+             sound.start();
+                  });
+         }); $('#stop5').click(function(e) {
+             e.preventDefault();
+             sound.stop();
+           });
+    });
+    $(document).ready(function() {  SC.stream('/tracks/488970675',function(sound){
+        $('#start6').click(function(e) {
+              e.preventDefault();
+              sound.start();
+                   });
+          }); $('#stop6').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+            });
+     });
+     $(document).ready(function() {  SC.stream('/tracks/45386331',function(sound){
+         $('#start7').click(function(e) {
+               e.preventDefault();
+               sound.start();
+                    });
+           }); $('#stop7').click(function(e) {
+               e.preventDefault();
+               sound.stop();
+             });
+      });
+      $(document).ready(function() {  SC.stream('/tracks/529335735',function(sound){
+          $('#start8').click(function(e) {
+                e.preventDefault();
+                sound.start();
+                     });
+            }); $('#stop8').click(function(e) {
+                e.preventDefault();
+                sound.stop();
+              });
+       });
+       $(document).ready(function() {  SC.stream('/tracks/400024665',function(sound){
+           $('#start9').click(function(e) {
+                 e.preventDefault();
+                 sound.start();
+                      });
+             }); $('#stop9').click(function(e) {
+                 e.preventDefault();
+                 sound.stop();
+               });
+        });
+        $(document).ready(function() {  SC.stream('/tracks/318224589',function(sound){
+            $('#start10').click(function(e) {
+                  e.preventDefault();
+                  sound.start();
+                       });
+              }); $('#stop10').click(function(e) {
+                  e.preventDefault();
+                  sound.stop();
+                });
+         });
 });
